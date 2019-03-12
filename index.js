f4564747cff9278979e3911490bab3ae2b34e902
Xiaoxuan Li <lixiaoxuanvip@gmail.com>
2019-03-12 11:29:27 -0700

    add profile

--- index.js
+++ index.js
@@ -5,7 +5,6 @@ function onSignIn(googleUser){
     $("#pic").attr('src', profile.getImageUrl());
     $("#email").text(profile.getEmail());
 }
-
 function signOut(){
     var auth2 = gapi.auth2.getAuthInstance();
     auth2.signOut().then(function(){
